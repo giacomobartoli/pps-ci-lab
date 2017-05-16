05987b4ef46557f3a74b38e1c1a94d8b683e3874
Marco Baldassarri <marco.baldassarri2@studio.unibo.it>
2017-05-16 14:53:35 +0200

    created HelloWorld

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,8 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Marco on 16/05/2017.
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
